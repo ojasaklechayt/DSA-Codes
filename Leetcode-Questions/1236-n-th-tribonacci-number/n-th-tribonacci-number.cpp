1e24b51405560ea5d9f58ea0f3ec3035a6f3f1d6
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-04-24 21:44:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (53.59%) - LeetSync

--- Leetcode-Questions/1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ Leetcode-Questions/1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -3,15 +3,18 @@ public:
     int tribonacci(int n) {
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-        vector<int> dp(n+1);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
 
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        
         for (int i = 3; i <= n; ++i) {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            int next = a + b + c;
+            a = b;
+            b = c;
+            c = next;
         }
 
-        return dp[n];
+        return c;
     }
 };

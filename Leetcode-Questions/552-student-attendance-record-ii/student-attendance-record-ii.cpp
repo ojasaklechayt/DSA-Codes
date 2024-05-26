5a69870a015e6ad270365fb9e679abcfe834c9fe
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-05-26 12:42:16 +0530

    Time: 1091 ms (33.54%) | Memory: 386.8 MB (41.77%) - LeetSync

--- Leetcode-Questions/552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ Leetcode-Questions/552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     int checkRecord(int n) {
         const int MOD = 1e9 + 7;
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
-        return countRecords(0, 0, 0, n, dp, MOD);
-    }
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        dp[0][0][0] = 1;
 
-private:
-    int countRecords(int i, int a, int l, int n, vector<vector<vector<int>>>& dp, const int MOD) {
-        if (i == n) return 1;
-        if (dp[i][a][l] != -1) return dp[i][a][l];
+        for (int i = 1; i <= n; ++i) {
+            for (int a = 0; a <= 1; ++a) {
+                for (int l = 0; l <= 2; ++l) {
+                    // Student is present
+                    dp[i][a][0] = (dp[i][a][0] + dp[i - 1][a][l]) % MOD;
+                    if (l < 2) dp[i][a][l + 1] = (dp[i][a][l + 1] + dp[i - 1][a][l]) % MOD;
+
+                    // Student is absent
+                    if (a < 1) dp[i][a + 1][0] = (dp[i][a + 1][0] + dp[i - 1][a][l]) % MOD;
+                }
+            }
+        }
 
         int ans = 0;
+        for (int a = 0; a <= 1; ++a) {
+            for (int l = 0; l <= 2; ++l) {
+                ans = (ans + dp[n][a][l]) % MOD;
+            }
+        }
 
-        // Student is present
-        ans = (ans + countRecords(i + 1, a, 0, n, dp, MOD)) % MOD;
-
-        // Student is late
-        if (l < 2) ans = (ans + countRecords(i + 1, a, l + 1, n, dp, MOD)) % MOD;
-
-        // Student is absent
-        if (a < 1) ans = (ans + countRecords(i + 1, a + 1, 0, n, dp, MOD)) % MOD;
-
-        dp[i][a][l] = ans;
         return ans;
     }
 };

7b29453a969daac92959a39a4c5627a9f552fe5d
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-05-31 23:37:12 +0530

    Time: 3 ms (66.58%) | Memory: 12.1 MB (10.11%) - LeetSync

--- Leetcode-Questions/74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ Leetcode-Questions/74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,18 +1,53 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+
+//         int row = 0;
+//         int col = m-1;
+//         while(row<n && col>=0){
+//             if(matrix[row][col] == target){
+//                 return true;
+//             } else if (target > matrix[row][col]){
+//                 row++;
+//             } else {
+//                 col--;
+//             }
+//         }
+
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
 
-        int row = 0;
-        int col = m-1;
-        while(row<n && col>=0){
-            if(matrix[row][col] == target){
+        for(int i=0; i<n; i++){
+            if(booleancheck(matrix[i] , target)){
                 return true;
-            } else if (target > matrix[row][col]){
-                row++;
+            }
+        }
+
+        return false;
+    }
+
+private:
+    bool booleancheck(vector<int> row,  int target){
+        int start = 0;
+        int end = row.size()-1;
+
+        while(start <= end){  
+            int mid = start + (end-start)/2;
+            if(row[mid] == target){
+                return true;
+            } else if (row[mid] < target){
+                start = mid + 1;
             } else {
-                col--;
+                end = mid - 1;
             }
         }
 

5f97c243bdc227f89ea147393e3d4dacba00fd6e
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-06-09 12:43:36 +0530

    Time: 40 ms (15.50%) | Memory: 38.6 MB (22.40%) - LeetSync

--- Leetcode-Questions/653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ Leetcode-Questions/653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,35 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    // Main function to find if there exist two elements in the BST that add up
+    // to the given target.
     bool findTarget(TreeNode* root, int k) {
+        // Initialize a map to store complements.
         unordered_map<int, int> complement;
+        // Call helper function inorder() to traverse the BST and find the
+        // target sum.
         return inorder(root, k, complement);
     }
 
 private:
-    bool inorder(TreeNode* root, int k, unordered_map<int, int>& complement){
-        if(root == NULL) return false;
+    // Helper function to perform inorder traversal and find the target sum.
+    bool inorder(TreeNode* root, int k, unordered_map<int, int>& complement) {
+        // If the current node is NULL, return false.
+        if (root == NULL)
+            return false;
 
-        if(complement.find(root->val) != complement.end()){
+        // If the complement of the current node's value is found in the map,
+        // return true.
+        if (complement.find(root->val) != complement.end()) {
             return true;
         }
 
+        // Calculate the complement of the current node's value.
         int complem = k - (root->val);
 
+        // Insert the complement into the map with the current node's value as
+        // the corresponding value.
         complement[complem] = root->val;
 
-        if(inorder(root->left, k, complement)) return true;
+        // Recursively check the left subtree. If a pair is found, return true.
+        if (inorder(root->left, k, complement))
+            return true;
 
+        // Recursively check the right subtree. If a pair is found, return true.
         return inorder(root->right, k, complement);
     }
 };

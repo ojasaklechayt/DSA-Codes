db26510c257bcaf73e80ba4717956bffdaae8f10
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-06-10 13:28:12 +0530

    Time: 132 ms (85.16%) | Memory: 101.4 MB (80.55%) - LeetSync

--- Leetcode-Questions/1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ Leetcode-Questions/1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -1,34 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+class info{
+ public:
+    int maxi;
+    int mini;
+    bool isbst;
+    int sum;
+ };
+    info solve(TreeNode* root , int &ans){
+           if( root ==NULL){
+                 return {INT_MIN , INT_MAX   , 1 , 0}; }
+           if( root->left ==NULL && root->right ==NULL){
+                ans = max(ans , root->val);
+                 return {root->val , root->val , true , root->val};
+            }
+
+           info leftans = solve(root->left  , ans);
+           info rightans = solve( root->right , ans);
+
+             info currentnode;
+             currentnode.maxi = max(root->val , rightans.maxi);
+             currentnode.mini  = min(root->val  , leftans.mini);
+             currentnode.sum = root->val + rightans.sum + leftans.sum;
+
+             if( leftans.isbst && rightans.isbst && ( root->val > leftans.maxi &&              root->val<rightans.mini)){
+                   currentnode.isbst = true;}
+             else{
+                   currentnode.isbst = false;
+             }
+             if(currentnode.isbst){
+                   ans = max(ans, currentnode.sum);
+             }
+             return currentnode;
+    }
 public:
     int maxSumBST(TreeNode* root) {
-        int maxSum = 0;
-        maxSumBSTHelper(root, maxSum);
-        return maxSum;
-    }
-
-private:
-    struct Result {
-        bool isBST;
-        int sum;
-        int minVal;
-        int maxVal;
-    };
-
-    Result maxSumBSTHelper(TreeNode* root, int& maxSum) {
-        if (root == nullptr)
-            return {true, 0, INT_MAX, INT_MIN};
-
-        Result left = maxSumBSTHelper(root->left, maxSum);
-        Result right = maxSumBSTHelper(root->right, maxSum);
-
-        if (left.isBST && right.isBST && root->val > left.maxVal &&
-            root->val < right.minVal) {
-            int sum = left.sum + right.sum + root->val;
-            maxSum = std::max(maxSum, sum);
-            return {true, sum, std::min(root->val, left.minVal),
-                    std::max(root->val, right.maxVal)};
-        } else {
-            return {false, 0, 0, 0};
-        }
+               int ans = 0;
+           solve( root , ans );
+          return ans;
     }
 };

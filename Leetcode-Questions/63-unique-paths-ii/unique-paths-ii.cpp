e86e53088a56ec7840a1e67a8f62c55fc579bfec
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-06-20 14:34:28 +0530

    Time: 5 ms (36.82%) | Memory: 10.2 MB (63.32%) - LeetSync

--- Leetcode-Questions/63-unique-paths-ii/unique-paths-ii.cpp
+++ Leetcode-Questions/63-unique-paths-ii/unique-paths-ii.cpp
@@ -31,45 +31,88 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+//         int n = obstacleGrid.size();
+//         int m = obstacleGrid[0].size();
+
+//         if (obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) {
+//             return 0;
+//         }
+
+//         memo = vector<vector<int>>(n, vector<int>(m, -1));
+
+//         return dfs(0, 0, obstacleGrid);
+//     }
+
+// private:
+//     vector<vector<int>> memo;
+
+//     int dfs(int i, int j, vector<vector<int>>& obstacleGrid){
+//         int n = obstacleGrid.size();
+//         int m = obstacleGrid[0].size();
+
+//         if (i >= n || j >= m || obstacleGrid[i][j] == 1) {
+//             return 0;
+//         }
+
+//         if(i == n-1 && j == m-1){
+//             return 1;
+//         }
+        
+//         if (memo[i][j] != -1) {
+//             return memo[i][j];
+//         }
+
+//         int left = dfs(i+1, j,obstacleGrid);
+//         int right = dfs(i, j+1,obstacleGrid);
+        
+//         memo[i][j] = left + right;
+
+//         return memo[i][j];
+//     }
+// };
+
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
 
+        // If the starting point or the ending point is an obstacle, return 0 paths.
         if (obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) {
             return 0;
         }
 
-        memo = vector<vector<int>>(n, vector<int>(m, -1));
-
-        return dfs(0, 0, obstacleGrid);
-    }
-
-private:
-    vector<vector<int>> memo;
-
-    int dfs(int i, int j, vector<vector<int>>& obstacleGrid){
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
-
-        if (i >= n || j >= m || obstacleGrid[i][j] == 1) {
-            return 0;
-        }
-
-        if(i == n-1 && j == m-1){
-            return 1;
-        }
+        vector<vector<int>> dp(n, vector<int>(m, 0));
         
-        if (memo[i][j] != -1) {
-            return memo[i][j];
+        // Base case: There's one way to be at the starting point.
+        dp[0][0] = 1;
+
+        // Fill the first row
+        for (int j = 1; j < m; ++j) {
+            if (obstacleGrid[0][j] == 0) {
+                dp[0][j] = dp[0][j-1];
+            }
         }
 
-        int left = dfs(i+1, j,obstacleGrid);
-        int right = dfs(i, j+1,obstacleGrid);
-        
-        memo[i][j] = left + right;
+        // Fill the first column
+        for (int i = 1; i < n; ++i) {
+            if (obstacleGrid[i][0] == 0) {
+                dp[i][0] = dp[i-1][0];
+            }
+        }
 
-        return memo[i][j];
+        // Fill the rest of the dp table
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[n-1][m-1];
     }
 };

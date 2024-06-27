1d0907ce308260f98030d9dee7c014718d2217e7
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-06-27 13:11:05 +0530

    Time: 131 ms (75.08%) | Memory: 70.7 MB (92.91%) - LeetSync

--- Leetcode-Questions/1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ Leetcode-Questions/1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,18 +1,31 @@
+// class Solution {
+// public:
+//     int findCenter(vector<vector<int>>& edges) {
+//         int n = edges.size() + 1;
+//         vector<vector<int>> adjlist(n+1);
+//         for(auto& i: edges){
+//             adjlist[i[0]].push_back(i[1]);
+//             adjlist[i[1]].push_back(i[0]);
+//         } 
+
+//         for(int i=0; i<adjlist.size(); i++){
+//             int t = adjlist[i].size();
+//             if(t == n-1) return i;
+//         }
+
+//         return 0;
+//     }
+// };
+
+
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size() + 1;
-        vector<vector<int>> adjlist(n+1);
-        for(auto& i: edges){
-            adjlist[i[0]].push_back(i[1]);
-            adjlist[i[1]].push_back(i[0]);
-        } 
-
-        for(int i=0; i<adjlist.size(); i++){
-            int t = adjlist[i].size();
-            if(t == n-1) return i;
+        // Check the common node between the first two edges
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        } else {
+            return edges[0][1];
         }
-
-        return 0;
     }
 };

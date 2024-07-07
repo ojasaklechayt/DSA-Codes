1340ad2a8a5f0a383b81a3b6ffb7327aacb816cb
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-07-07 10:28:10 +0530

    Time: 2 ms (53.84%) | Memory: 7.2 MB (30.43%) - LeetSync

--- Leetcode-Questions/1642-water-bottles/water-bottles.cpp
+++ Leetcode-Questions/1642-water-bottles/water-bottles.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
         int total = numBottles;
-        while(numBottles >= numExchange){
+        while(true){
             total += numBottles/numExchange;
             numBottles = numBottles/numExchange + numBottles%numExchange;
+            if(numBottles < numExchange){
+                break;
+            }
         }
 
         return total;

05052760c395a1a00834c5d2fe604dd4e4bb8f8a
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-07-19 16:57:33 +0530

    Time: 95 ms (58.29%) | Memory: 64.6 MB (5.49%) - LeetSync

--- Leetcode-Questions/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ Leetcode-Questions/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> mean;
-        for(int num: nums) {
-            mean.push(num);
-            if(mean.size() > k){
-                mean.pop();
-            }
+        priority_queue<int> qt;
+        for(int i=0; i<nums.size(); i++){
+            qt.push(nums[i]);
         }
 
-        return mean.top();
+        for(int i=0; i<k-1; i++){
+            qt.pop();
+        }
+
+        return qt.top();
     }
 };

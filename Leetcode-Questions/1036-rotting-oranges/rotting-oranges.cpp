979df9c215d8618e5b5b08c887b6853e17d4f62f
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-07-22 17:00:23 +0530

    Time: 7 ms (42.69%) | Memory: 16.4 MB (24.40%) - LeetSync

--- Leetcode-Questions/1036-rotting-oranges/rotting-oranges.cpp
+++ Leetcode-Questions/1036-rotting-oranges/rotting-oranges.cpp
@@ -3,48 +3,50 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
-        bool anyChange = false;
 
-        do {
-            anyChange = false;
-            vector<vector<int>> newGrid = grid;
+        queue<pair<int, int>> qt;
+        int freshCount = 0;
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (grid[i][j] == 2) {
-                        if (rot(i + 1, j, grid, newGrid)) anyChange = true;
-                        if (rot(i - 1, j, grid, newGrid)) anyChange = true;
-                        if (rot(i, j + 1, grid, newGrid)) anyChange = true;
-                        if (rot(i, j - 1, grid, newGrid)) anyChange = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    qt.push({i, j});
+                } else if (grid[i][j] == 1) {
+                    freshCount++;
+                }
+            }
+        }
+
+        if (freshCount == 0)
+            return 0;
+
+        int mins = 0;
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        while (!qt.empty()) {
+            int size = qt.size();
+            bool rottenThisMinutes = false;
+
+            for (int i = 0; i < size; i++) {
+                auto [x, y] = qt.front();
+                qt.pop();
+
+                for(auto [ax, bx]: directions){
+                    int nx = x + ax;
+                    int ny = y + bx;
+
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 1){
+                        grid[nx][ny] = 2;
+                        qt.push({nx, ny});
+                        freshCount--;
+                        rottenThisMinutes = true;
                     }
                 }
             }
 
-            if (anyChange) {
-                grid = newGrid;
-                count++;
-            }
-        } while (anyChange);
-
-        // Check if there are any fresh oranges left.
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) return -1;
-            }
+            if(rottenThisMinutes) mins++;
         }
 
-        return count;
-    }
-
-private:
-    bool rot(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& newGrid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        if (i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == 1) {
-            newGrid[i][j] = 2;
-            return true;
-        }
-        return false;
+        return freshCount == 0 ? mins : -1;
     }
 };

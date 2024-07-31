fe05993b6cf67febbca6cc9507b70ac5b89239b1
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-07-31 12:51:24 +0530

    Time: 9 ms (17.02%) | Memory: 12.6 MB (42.84%) - LeetSync

--- Leetcode-Questions/1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
+++ Leetcode-Questions/1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -31,23 +31,51 @@
 
 
 
+// class Solution {
+// public:
+//     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+//         int n = books.size();
+//         vector<int>dp(n+1, INT_MAX);
+//         dp[0]=0;
+
+//         for(int i=1; i<=n; i++){
+//             int width = 0, height = 0;
+//             for(int j=i; j>0; j--){
+//                 width+=books[j-1][0];
+//                 if(width>shelfWidth) break;
+//                 height = max(height, books[j-1][1]);
+//                 dp[i] = min(dp[i], dp[j-1] + height);
+//             }
+//         }
+
+//         return dp[n];
+//     }
+// };
+
 class Solution {
 public:
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
-        int n = books.size();
-        vector<int>dp(n+1, INT_MAX);
-        dp[0]=0;
+        unordered_map<int,int> memo;
+        return helper(0, shelfWidth, books, memo);
+    }
 
-        for(int i=1; i<=n; i++){
-            int width = 0, height = 0;
-            for(int j=i; j>0; j--){
-                width+=books[j-1][0];
-                if(width>shelfWidth) break;
-                height = max(height, books[j-1][1]);
-                dp[i] = min(dp[i], dp[j-1] + height);
-            }
+private:
+    int helper(int i, int shelfWidth, vector<vector<int>>& books, unordered_map<int,int>& memo){
+        if(i == books.size()) return 0;
+        if (memo.find(i) != memo.end()) return memo[i];
+
+        int curr_width = shelfWidth, max_height = 0, result = INT_MAX;
+
+        for(int j=i; j<books.size(); j++){
+            int width = books[j][0], height = books[j][1];
+            
+            if(width > curr_width) break;
+            curr_width-=width;
+            max_height = max(max_height, height);
+            result = min(result, max_height + helper(j+1, shelfWidth, books, memo));
         }
 
-        return dp[n];
+        memo[i] = result;
+        return result;
     }
 };

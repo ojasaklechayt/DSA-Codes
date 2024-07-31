0f6d593367b6885852d6b4a9bbaa53d44c3856a3
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-07-31 14:42:29 +0530

    Time: 4 ms (31.56%) | Memory: 9.9 MB (37.70%) - LeetSync

--- Leetcode-Questions/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ Leetcode-Questions/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -9,19 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         helper(root, result);
+//         return result;
+//     }
+
+// private:
+//     void helper(TreeNode* root, vector<int>& result){
+//         if(root == nullptr) return;
+//         result.push_back(root->val);
+//         helper(root->left, result);
+//         helper(root->right, result);
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> result;
-        helper(root, result);
-        return result;
-    }
+        TreeNode* curr = root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                result.push_back(curr->val);
+                curr=curr->right;
+            } else {
+                TreeNode* prev = curr->left;
 
-private:
-    void helper(TreeNode* root, vector<int>& result){
-        if(root == nullptr) return;
-        result.push_back(root->val);
-        helper(root->left, result);
-        helper(root->right, result);
+                while(prev->right && prev->right!=curr){
+                    prev = prev->right;
+                }
+
+                if(prev->right==NULL){
+                    prev->right = curr;
+                    result.push_back(curr->val);
+                    curr = curr->left;
+                } else {
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return result;
     }
 };

81a18bbfaa0e95dada9a967a7ed6a1f9a156cf32
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-08-02 15:23:32 +0530

    Time: 8 ms (76.80%) | Memory: 9.7 MB (92.59%) - LeetSync

--- Leetcode-Questions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ Leetcode-Questions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,38 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n = s.size(), length = 0;
+
+//         for(int i=0; i<n; i++){
+//             unordered_set<char> hs;
+//             for(int j=i; j<n; j++){
+//                 if(hs.find(s[j]) != hs.end()){
+//                     break;
+//                 }
+//                 hs.insert(s[j]);
+//                 length = max(length, j - i + 1);
+//             }
+//         }
+
+//         return length;
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size(), length = 0;
+        int n = s.size(), length = 0, right = 0, left = 0;
 
-        for(int i=0; i<n; i++){
-            unordered_set<char> hs;
-            for(int j=i; j<n; j++){
-                if(hs.find(s[j]) != hs.end()){
-                    break;
-                }
-                hs.insert(s[j]);
-                length = max(length, j - i + 1);
+        string temp = "";
+        while(right<n){
+            if(temp.find(s[right])!=string::npos){
+                temp.erase(0,1);
+                left++;
+            } else{
+                temp+=s[right];
+                right++;
             }
+            length = max(length, right-left);
         }
 
         return length;

1f5a3fc49dba868be2d7dfd20f5ba157fa2428ce
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-08-03 12:18:01 +0530

    Time: 77 ms (94.25%) | Memory: 70.3 MB (55.69%) - LeetSync

--- Leetcode-Questions/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ Leetcode-Questions/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,54 +1,25 @@
-// class Solution {
-// public:
-//     int numberOfSubarrays(vector<int>& nums, int k) {
-//         int n = nums.size();
-//         int count = 0;
-        
-//         for(int i = 0; i < n; i++) {
-//             int odd = 0;
-//             for(int j = i; j < n; j++) {
-//                 if(nums[j] % 2 != 0) {
-//                     ++odd;
-//                 }
-                
-//                 if(odd == k) {
-//                     ++count;
-//                 } else if (odd > k) {
-//                     break;
-//                 }
-//             }
-//         }
-        
-//         return count;
-//     }
-// };
-
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> oddIndices;
-        oddIndices.push_back(-1); // Dummy index to handle edge cases
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] % 2 != 0) {
-                oddIndices.push_back(i);
+        return check(nums, k) - check(nums, k - 1);
+    }
+
+private:
+    int check(vector<int>& nums, int k) {
+        int n = nums.size(), left = 0, nice = 0;
+        int odd = 0;
+        for (int right = 0; right < n; right++) {
+            if (nums[right] % 2 != 0)
+                odd++;
+
+            while (odd > k) {
+                if (nums[left] % 2 != 0)
+                    odd--;
+                left++;
             }
-        }
-        oddIndices.push_back(n); // Dummy index to handle edge cases
 
-        int count = 0;
-        int m = oddIndices.size();
-        
-        if (m <= k + 1) {
-            return 0; // If there are fewer than k+1 odd numbers, return 0
+            nice += (right - left + 1);
         }
-
-        for (int i = 1; i + k < m; ++i) {
-            int leftCount = oddIndices[i] - oddIndices[i - 1];
-            int rightCount = oddIndices[i + k] - oddIndices[i + k - 1];
-            count += leftCount * rightCount;
-        }
-
-        return count;
+        return nice;
     }
 };

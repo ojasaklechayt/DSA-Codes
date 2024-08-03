540673e358dd304e69f9aa42ab8240b713e1e8ab
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-08-03 11:25:30 +0530

    Time: 3 ms (98.33%) | Memory: 17.5 MB (42.71%) - LeetSync

--- Leetcode-Questions/1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ Leetcode-Questions/1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,6 +1,21 @@
+// class Solution {
+// public:
+//     bool canBeEqual(vector<int>& target, vector<int>& arr) {
+//         return is_permutation(target.begin(), target.end(), arr.begin());
+//     }
+// };
+
+
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        return is_permutation(target.begin(), target.end(), arr.begin());
+        sort(arr.begin(), arr.end());
+        sort(target.begin(), target.end());
+        
+        for(int i=0; i<target.size(); i++){
+            if(arr[i] != target[i]) return false;
+        }
+
+        return true;
     }
 };

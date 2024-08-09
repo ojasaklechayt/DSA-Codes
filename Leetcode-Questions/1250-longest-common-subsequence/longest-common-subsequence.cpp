75eed991ce5284a8fdea012a39a673a7ea978236
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2024-08-09 11:44:17 +0530

    Time: 24 ms (60.57%) | Memory: 26 MB (33.43%) - LeetSync

--- Leetcode-Questions/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ Leetcode-Questions/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -21,6 +21,35 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int n, m;
+//     vector<vector<int>> dp;
+
+//     int longestCommonSubsequence(string text1, string text2) {
+//         n = text1.size(), m = text2.size();
+//         dp.resize(n, vector<int>(m, -1));
+//         return lcs(text1, text2, 0, 0);
+//     }
+
+//     int lcs(string &text1, string &text2, int ind1, int ind2) {
+//         if (ind1 >= n || ind2 >= m) {
+//             return 0;
+//         }
+
+//         if (dp[ind1][ind2] != -1) {
+//             return dp[ind1][ind2];
+//         }
+
+//         if (text1[ind1] == text2[ind2]) {
+//             return dp[ind1][ind2] = 1 + lcs(text1, text2, ind1 + 1, ind2 + 1);
+//         }
+
+//         return dp[ind1][ind2] = max(lcs(text1, text2, ind1 + 1, ind2), 
+//                                     lcs(text1, text2, ind1, ind2 + 1));
+//     }
+// };
+
 
 class Solution {
 public:
@@ -29,24 +58,15 @@ public:
 
     int longestCommonSubsequence(string text1, string text2) {
         n = text1.size(), m = text2.size();
-        dp.resize(n, vector<int>(m, -1));
-        return lcs(text1, text2, 0, 0);
-    }
+        dp.resize(n+1, vector<int>(m+1, 0));
 
-    int lcs(string &text1, string &text2, int ind1, int ind2) {
-        if (ind1 >= n || ind2 >= m) {
-            return 0;
+        for(int i=1; i<=n; ++i){
+            for(int j=1; j<=m; ++j){
+                if(text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
         }
 
-        if (dp[ind1][ind2] != -1) {
-            return dp[ind1][ind2];
-        }
-
-        if (text1[ind1] == text2[ind2]) {
-            return dp[ind1][ind2] = 1 + lcs(text1, text2, ind1 + 1, ind2 + 1);
-        }
-
-        return dp[ind1][ind2] = max(lcs(text1, text2, ind1 + 1, ind2), 
-                                    lcs(text1, text2, ind1, ind2 + 1));
+        return dp[n][m];
     }
 };
